7b2ee06dfa5e3fe5506cf0daca3aa2c6d4c63754
zhang <spade9k@gmail.com>
2015-11-16 16:20:10 +0800

    Update  constructor of Crawler.

--- test/crawler.js
+++ test/crawler.js
@@ -4,18 +4,20 @@ var Crawler = require('../lib/index'),
 	$ = require('cheerio'),
 	expect = require('chai').expect;
 
+// http-proxy
+var proxy = require('../lib/proxy').proxy;
+
 describe('Crawler', function () {
 	describe('#crwal()', function () {
-		this.timeout(10000);
+		this.timeout(30000);
 
 		var c;
-
 		beforeEach(function () {
-			c = new Crawler();
+			c = new Crawler({proxy: proxy});
 		});
 
 		it('simple test: single url', function (done) {
-			var url = 'http://www.baidu.com';
+			var url = 'http://www.google.com';
 			c.addTask(url);
 			c.handle = function (data) {
 				expect(data).to.be.ok;
@@ -26,7 +28,8 @@ describe('Crawler', function () {
 		});
 
 		it('simple test: url array', function (done) {
-			var urls = ['http://www.baidu.com', 'http://www.google.com'];
+			var urls = ['http://www.baidu.com', 'http://www.google.com', 
+						'http://www.sina.com', 'http://www.nhk.or.jp'];
 			c.addTasks(urls);
 			c.handle = function (data) {
 				expect(data).to.be.ok;
