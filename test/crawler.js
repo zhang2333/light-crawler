555083320d9bc38295167f2b6f39f32b5517cacb
zhang <spade9k@gmail.com>
2015-11-20 21:53:08 +0800

    Add log of failed task.

--- test/crawler.js
+++ test/crawler.js
@@ -9,7 +9,7 @@ var proxy = require('../t/proxy').proxy;
 
 describe('Crawler', function () {
 	describe('#crwal()', function () {
-		this.timeout(30000);
+		this.timeout(50000);
 
 		var c;
 		beforeEach(function () {
@@ -18,8 +18,9 @@ describe('Crawler', function () {
 
 		it('simple test: single url', function (done) {
 			var url = 'http://www.google.com';
-			c.addTasks(url).addRule(function (data) {
+			c.addTasks(url).addRule(function (data, callback) {
 				expect(data).to.be.ok;
+				callback();
 			}).start(function () {
 				done();
 			});
@@ -28,8 +29,9 @@ describe('Crawler', function () {
 		it('test timeout retry', function (done) {
 			var url = 'http://www.google.com';
 			c.settings.timeout = 10;
-			c.addTasks(url).addRule(function (data) {
-				expect(data).to.be.ok;
+			c.addTasks(url).addRule(function (data, callback) {
+				expect(data).to.not.be.ok;
+				callback();
 			}).start(function () {
 				done();
 			});
@@ -39,8 +41,9 @@ describe('Crawler', function () {
 			var urls = ['http://www.baidu.com', 'http://www.google.com',
 				'http://www.sina.com', 'http://www.nhk.or.jp'];
 			c.twistSettings({ interval: 1500, concurrency: 2 });
-			c.addTasks(urls).addRule(function (data) {
+			c.addTasks(urls).addRule(function (data, callback) {
 				expect(data).to.be.ok;
+				callback();
 			}).start(function () {
 				done();
 			});
@@ -50,13 +53,26 @@ describe('Crawler', function () {
 			var urls = ['http://www.baidu.com', 'http://www.baidu.com',
 				'http://www.baidu.com', 'http://www.sina.com'];
 			c.settings.interval = 1500;
-			c.addTasks(urls).addRule(function (data) { }).start(function () {
-				expect(c.taskCounter).to.be.equal(2);
+			c.addTasks(urls).addRule(function (data, callback) { 
+				callback();
+			}).start(function () {
+				expect(c.taskCounter).to.be.equal(3);
 				done();
 			});
 			setTimeout(function () {
 				c.addTasks('http://www.baidu.com');
+				c.addTasks(['http://www.baidu.com', 'http://www.sohu.com']);
 			}, 1600);
+		});
+		
+		it('test retry failed', function (done) {
+			var urls = ['http://www.zhihu.com'];
+			c.addTasks(urls).addRule(function (data, callback) { 
+				callback();
+			}).start(function () {
+				expect(c.errLog).to.have.length.above(0);
+				done();
+			});
 		});
 	});
 });
