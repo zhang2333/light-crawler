e72fada30420adde022213a1d4d2ca45d396d9c8
zhang <spade9k@gmail.com>
2015-11-21 18:28:21 +0800

    Beautify code.

--- test/crawler.js
+++ test/crawler.js
@@ -12,15 +12,12 @@ describe('Crawler', function () {
 		this.timeout(50000);
 
 		var c;
-		beforeEach(function () {
-			c = new Crawler({ proxy: proxy });
-		});
 
 		it('simple test: single url', function (done) {
 			var url = 'http://www.google.com';
-			c.addTasks(url).addRule(function (data, callback) {
+			c = new Crawler({ proxy: proxy });
+			c.addTasks(url).addRule(function (data) {
 				expect(data).to.be.ok;
-				callback();
 			}).start(function () {
 				done();
 			});
@@ -28,10 +25,9 @@ describe('Crawler', function () {
 
 		it('test timeout retry', function (done) {
 			var url = 'http://www.google.com';
-			c.settings.timeout = 10;
-			c.addTasks(url).addRule(function (data, callback) {
+			c = new Crawler({ proxy: proxy, timeout: 10 });
+			c.addTasks(url).addRule(function (data) {
 				expect(data).to.not.be.ok;
-				callback();
 			}).start(function () {
 				done();
 			});
@@ -40,10 +36,9 @@ describe('Crawler', function () {
 		it('test array interval concurrency', function (done) {
 			var urls = ['http://www.baidu.com', 'http://www.google.com',
 				'http://www.sina.com', 'http://www.nhk.or.jp'];
-			c.twistSettings({ interval: 1500, concurrency: 2 });
-			c.addTasks(urls).addRule(function (data, callback) {
+			c = new Crawler({ proxy: proxy, interval: 1500, concurrency: 2 });
+			c.addTasks(urls).addRule(function (data) {
 				expect(data).to.be.ok;
-				callback();
 			}).start(function () {
 				done();
 			});
@@ -52,10 +47,8 @@ describe('Crawler', function () {
 		it('test repetitive tasks', function (done) {
 			var urls = ['http://www.baidu.com', 'http://www.baidu.com',
 				'http://www.baidu.com', 'http://www.sina.com'];
-			c.settings.interval = 1500;
-			c.addTasks(urls).addRule(function (data, callback) { 
-				callback();
-			}).start(function () {
+			c = new Crawler({ proxy: proxy, interval: 1500});
+			c.addTasks(urls).addRule(function (data) {}).start(function () {
 				expect(c.taskCounter).to.be.equal(3);
 				done();
 			});
@@ -63,16 +56,6 @@ describe('Crawler', function () {
 				c.addTasks('http://www.baidu.com');
 				c.addTasks(['http://www.baidu.com', 'http://www.sohu.com']);
 			}, 1600);
-		});
-		
-		it('test retry failed', function (done) {
-			var urls = ['http://www.zhihu.com'];
-			c.addTasks(urls).addRule(function (data, callback) { 
-				callback();
-			}).start(function () {
-				expect(c.errLog).to.have.length.above(0);
-				done();
-			});
 		});
 	});
 });
