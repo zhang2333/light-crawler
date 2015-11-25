256485f543c4e7b60e9bd3888f7e87519cd40c53
zhang <spade9k@gmail.com>
2015-11-25 20:26:17 +0800

    Update for publish.

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 ## Light Crawler - Simplified Crawler
+
+### How to install
+```shell
+npm install light-crawler
+```
+
+### Example
+```javascript
+// create a Crawler instance
+var Crawler = require('light-crawler');
+var c = new Crawler();
+// add a url or array of url for crawling
+c.addTasks('http://www.xxx.com');
+// define a crawling rule and processing funciton
+c.addRule(function (data) {
+	// data has 2 props : url and body
+	// data.body is the HTML of the crawling page
+	processing data.body... // you can use cheerio
+})
+// start your crawler
+c.start(function () {
+	console.log('Success!');
+});
+```
--- test/crawler.js
+++ test/crawler.js
@@ -6,9 +6,6 @@ var expect = require('chai').expect;
 
 var Crawler = require('../index');
 
-// http-proxy
-var proxy = require('../t/proxy').proxy;
-
 describe('Crawler', function () {
 	describe('#crwal()', function () {
 		this.timeout(30000);
@@ -17,8 +14,9 @@ describe('Crawler', function () {
 
 		it('simple test: single url', function (done) {
 			var url = 'http://www.google.com';
-			c = new Crawler({ proxy: proxy });
+			c = new Crawler();
 			c.addTasks(url).addRule(function (data) {
+				require('fs').writeFileSync('C:\\Users\\hp\\Desktop\\a.html', data.body);
 				expect(data.body).to.be.ok;
 			}).start(function () {
 				done();
@@ -27,7 +25,7 @@ describe('Crawler', function () {
 
 		it('test timeout retry', function (done) {
 			var url = 'http://www.google.com';
-			c = new Crawler({ proxy: proxy, timeout: 10 });
+			c = new Crawler({ timeout: 10 });
 			c.addTasks(url).addRule(function (data) {
 				expect(data.body).to.not.be.ok;
 			}).start(function () {
@@ -38,7 +36,7 @@ describe('Crawler', function () {
 		it('test array interval concurrency', function (done) {
 			var urls = ['http://www.baidu.com', 'http://www.google.com',
 				'http://www.sina.com', 'http://www.nhk.or.jp'];
-			c = new Crawler({ proxy: proxy, interval: 500, concurrency: 2 });
+			c = new Crawler({ interval: 500, concurrency: 2 });
 			c.addTasks(urls).addRule(function (data) {
 				expect(data.body).to.be.ok;
 			}).start(function () {
@@ -49,7 +47,7 @@ describe('Crawler', function () {
 		it('test repetitive tasks', function (done) {
 			var urls = ['http://www.baidu.com', 'http://www.baidu.com',
 				'http://www.baidu.com', 'http://www.sina.com'];
-			c = new Crawler({ proxy: proxy, interval: 1500 });
+			c = new Crawler({ interval: 1500 });
 			c.addTasks(urls).addRule(function (data) { }).start(function () {
 				expect(c.taskCounter).to.be.equal(3);
 				done();
